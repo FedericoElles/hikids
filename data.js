38a003b0b0b256745f2cd82aa585c4e4d0bcab27
Glitch (hikids) <none>
2017-12-06 22:42:30 +0000

    🍌🎤 Updated with Glitch

---
+++ data.js
@@ -0,0 +1,13 @@
+var videos = [{
+      title: "Valerie & Andreas",
+      video: "https://cdn.glitch.com/219b277d-fe56-4591-a008-b3ef549b97f4%2Fvalerie-andreas.mp4"
+    },{
+      title: "Mewes & Laura",
+      video: "https://cdn.glitch.com/219b277d-fe56-4591-a008-b3ef549b97f4%2Fmewes-laura.mp4"
+    },{
+      title: "Angelika",
+      video: "https://cdn.glitch.com/219b277d-fe56-4591-a008-b3ef549b97f4%2Fangelika.mp4"
+    },{
+      title: "England",
+      video: "https://cdn.glitch.com/219b277d-fe56-4591-a008-b3ef549b97f4%2Fengland.mp4"
+    }];
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('hi');
 
-
+//play video polaroids
 [].forEach.call(document.querySelectorAll('.c-videocard'), function( elem ) {
   elem.addEventListener('click', function() {
     elem.classList.add('is-active');
@@ -12,3 +12,27 @@ console.log('hi');
     
   });
 });
+
+
+//show screens
+[].forEach.call(document.querySelectorAll('.js-screen'), function( elem ) {
+  elem.addEventListener('click', function() {
+    var target = elem.attributes.target.value;
+    [].forEach.call(document.querySelectorAll('.c-screen'), function( elem ) {
+      elem.classList.remove('is-visible');
+    });
+    
+    document.querySelector('#' + target).classList.add('is-visible');
+    
+  });
+});
+
+
+var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
+             "{{kids.length}} kids:</p>" +
+             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+var template = Handlebars.compile(source);
+
+var data = { "name": "Alan", "hometown": "Somewhere, TX",
+             "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+var result = template(data);
